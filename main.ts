2f454c9290e06878bde055fa84b3665fc3d2a4c9
TheMaksoo <39918032+Moheax@users.noreply.github.com>
2021-10-12 10:36:07 +0200

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    let list: number[] = []
+    for (let waarde of list) {
+        music.playTone(waarde, music.beat(BeatFraction.Whole))
+    }
+})
+let lijst = [250, 500, 750, 1000, 1250, 1500]
+basic.forever(function () {
+	
+})
